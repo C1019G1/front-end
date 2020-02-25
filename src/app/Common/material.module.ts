cbe97f842525aa1b84e11eab3a2e725d71d52f1d
Shinya9082 <anhtuandtu1993@gmail.com>
2020-02-25 09:51:40 +0700

    Project add boostrap 4 and anguler marterial

---
+++ src/app/Common/material.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+  ],
+  exports: [
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+  ]
+})
+export class MaterialModule { }
--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ProductsComponent } from './products.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import {MaterialModule} from '../Common/material.module';
+import {Ng2Module} from '../Common/ng2.module';
 
 const routes: Routes = [{ path: '', component: ProductsComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild(routes),
+    MaterialModule,
+    Ng2Module,
+  ],
   exports: [RouterModule],
   declarations: [ProductListComponent]
 })
