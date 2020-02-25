b905972854e86d83cb88d15ea7b45cc052ec0e8f
Shinya9082 <anhtuandtu1993@gmail.com>
2020-02-25 11:47:36 +0700

    Project add  popper admin-user-manager.component init

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminComponent } from './admin.component';
+import { AdminUserManagerComponent } from './admin-user-manager/admin-user-manager.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  { path: '', component: AdminComponent },
+  { path: 'user-manager', component: AdminUserManagerComponent },
+  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [AdminUserManagerComponent]
 })
 export class AdminRoutingModule { }
