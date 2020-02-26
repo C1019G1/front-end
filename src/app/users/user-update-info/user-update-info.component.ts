405fa162610118a6c12fa84db48b68c9a6131b8f
buicongthanh-2801 <thanhbui280197@gmail.com>
2020-02-26 14:46:03 +0700

    user lịch sử đấu giá/cập nhật tài khoản/

--- src/app/users/user-update-info/user-update-info.component.ts
+++ src/app/users/user-update-info/user-update-info.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 
 
@@ -11,15 +11,28 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class UserUpdateInfoComponent implements OnInit {
   public formUpdateInfo: FormGroup;
   public idUser;
-
+  public maxDate = new Date();
+  public minDate = new Date(1900, 0, 1);
   constructor(
+    public formBuilder: FormBuilder,
+    public router: Router
   ) {
   }
 
   ngOnInit(): void {
-    // this.formUpdateInfo = this.formBuilder.group({
-
-    // });
+    this.formUpdateInfo = this.formBuilder.group({
+      userName: ['', [Validators.required]],
+      fullName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      dateOfBirth: ['', [Validators.required]],
+      currentPassword: ['', [Validators.required]],
+      newPassword: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+      identityNumber: ['', [Validators.required, Validators.pattern('^[0-9]{9}$')]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^(090|091|([\(]84[\)][\+]90)|([\(]84[\)][\+]91))([0-9]{7})$')]],
+      address: ['', [Validators.required]],
+    })
+    ;
   }
 
   // updateInfo() {
