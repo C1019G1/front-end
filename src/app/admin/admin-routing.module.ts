12c8da155f2f6299dca8cf0a2f072bf524f47c7e
Shinya9082 <anhtuandtu1993@gmail.com>
2020-02-26 08:37:57 +0700

    admin-user-create.component init

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -5,6 +5,7 @@ import { AdminComponent } from './admin.component';
 import { AdminUserManagerComponent } from './admin-user-manager/admin-user-manager.component';
 import {MaterialModule} from '../Common/material.module';
 import {Ng2Module} from '../Common/ng2.module';
+import { AdminUserCreateComponent } from './admin-user-create/admin-user-create.component';
 
 const routes: Routes = [
   { path: '', component: AdminComponent },
@@ -18,6 +19,6 @@ const routes: Routes = [
     Ng2Module,
   ],
   exports: [RouterModule],
-  declarations: [AdminUserManagerComponent]
+  declarations: [AdminUserManagerComponent, AdminUserCreateComponent]
 })
 export class AdminRoutingModule { }
