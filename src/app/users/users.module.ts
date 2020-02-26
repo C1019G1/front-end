41f99b3a16e1df38f8290d7d388c4e3e08a5966b
dungtranviet <dungtvz91@gmail.com>
2020-02-26 14:37:03 +0700

    register form

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
+import {NgxCaptchaModule} from 'ngx-captcha';
 
 
 @NgModule({
   declarations: [UsersComponent],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    NgxCaptchaModule
   ]
 })
 export class UsersModule { }
