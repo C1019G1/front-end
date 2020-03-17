a6bc8c4410c73bee92e34457208ca702517f797c
dungtranviet <dungtvz91@gmail.com>
2020-03-17 10:27:34 +0700

    quan li san pham

--- src/app/products/product-update/product-update.component.ts
+++ src/app/products/product-update/product-update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-product-update',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-update.component.css']
 })
 export class ProductUpdateComponent implements OnInit {
-
-  constructor() { }
+  productInfor: FormGroup;
+  minDate = new Date();
+  maxDate = new Date();
+  productInfort: ProductInfor =  {name: 'Iphone 8', userId: '1', userInforDetail: 'Id : 12', catalogue: 'Điện tử', start_price: '120000'
+    , min_bet: '100000', start_day: '23/3/2020', end_day: '26/3/2020', product_infor: 'Sản xuất năm 2018'};
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.productInfor = this.fb.group({
+      name: ['', [Validators.required]],
+      userId: ['', Validators.required],
+      userInforDetail: ['', Validators.required],
+      catalogue: ['', Validators.required],
+      start_price: ['', Validators.required, Validators.pattern('^[0-9]$')],
+      min_bet: ['', Validators.required, Validators.pattern('^[0-9]$')],
+      start_day: ['', Validators.required],
+      end_day: ['', Validators.required],
+      product_infor: ['', Validators.required],
+    });
+    this.productInfor.patchValue(this.productInfort);
   }
-
+  onSubmit() {
+  }
 }
+export interface ProductInfor {
+  name: string;
+  userId: string;
+  userInforDetail: string;
+  catalogue: string;
+  start_price: string;
+  min_bet: string;
+  start_day: string;
+  end_day: string;
+  product_infor: string;
+}
+
+
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import {MaterialModule} from '../Common/material.module';
+import {ReactiveFormsModule} from '@angular/forms';
+import { ProductUpdateComponent } from './product-update/product-update.component';
 
 @NgModule({
-  declarations: [ProductsComponent, ],
+  declarations: [ProductsComponent, ProductAddComponent, ProductUpdateComponent, ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductsModule { }
