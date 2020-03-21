fbcc010beac9063a0200e8f13af44cf1b08ce92c
Shinya9082 <anhtuandtu1993@gmail.com>
2020-03-21 16:04:50 +0700

    drag and drop init

--- src/app/products/product-register/product-register.component.ts
+++ src/app/products/product-register/product-register.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 export interface ProductCatalogue {
   id;
   name;
 }
+
 @Component({
   selector: 'app-product-register',
   templateUrl: './product-register.component.html',
   styleUrls: ['./product-register.component.css']
 })
 export class ProductRegisterComponent implements OnInit {
+  files: File[] = [];
+  imgUrlList = [];
   today = new Date();
   productRegisterForm: FormGroup;
   productCatalogue: ProductCatalogue[] = [];
+
   constructor(
     private formBuilder: FormBuilder
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.productRegisterForm = this.formBuilder.group({
@@ -25,6 +30,18 @@ export class ProductRegisterComponent implements OnInit {
     });
   }
 
+  uploadFile(files: FileList) {
+    for (let i = 0; i < files.length; i++) {
+      const file = files.item(i);
+      if (this.files.findIndex(data => data.name === file.name) < 0) {
+        this.files.push(file);
+      }
+    }
+  }
+
+  deleteAttachment(index) {
+    this.files.splice(index, 1);
+  }
 
   onSendClick() {
 
