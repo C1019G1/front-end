54acb2f6b6689e5d4d65dda8f6a6288f2306bdce
Shinya9082 <anhtuandtu1993@gmail.com>
2020-03-22 11:27:28 +0700

    get real time data

--- src/app/products/poduct-detail/poduct-detail.component.ts
+++ src/app/products/poduct-detail/poduct-detail.component.ts
@@ -1,17 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ProductRegisterService} from '../../services/product-register.service';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-poduct-detail',
   templateUrl: './poduct-detail.component.html',
   styleUrls: ['./poduct-detail.component.css']
 })
-export class PoductDetailComponent implements OnInit {
+export class PoductDetailComponent implements OnInit, OnDestroy {
   public formAuction: FormGroup;
   public productDetail;
   public productOfId;
+  currentPrice: Observable<any>;
+  topFive = [];
+  interval: any;
+  count = 0;
 
   constructor(
     public productRegisterService: ProductRegisterService,
@@ -34,7 +39,29 @@ export class PoductDetailComponent implements OnInit {
         console.log(this.productDetail);
       });
     });
+    this.getCurrentPrice(this.productOfId);
+    this.interval = setInterval(() => {
+      this.getCurrentPrice(this.productOfId);
+      this.getTopFive(this.productOfId);
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  getCurrentPrice(id: number) {
+    this.currentPrice = this.productRegisterService.getCurrentPriceById(id);
+  }
+  getTopFive(id: number) {
+     this.productRegisterService.getTopFive(id).subscribe(value => {
+       this.topFive = value;
+     });
+  }
+  auction() {
 
   }
+
+
 }
 
--- src/app/services/product-register.service.ts
+++ src/app/services/product-register.service.ts
@@ -12,7 +12,7 @@ export class ProductRegisterService {
   }
 
   getAllProduct(page: number, catalogue: string): Observable<any> {
-    return this.http.get(this.API + '/list?page=' + page + '&size=6&&catalogue=' + catalogue );
+    return this.http.get(this.API + '/list?page=' + page + '&size=6&&catalogue=' + catalogue);
   }
 
   getProductById(productId: number): Observable<any> {
@@ -22,9 +22,17 @@ export class ProductRegisterService {
   searchByNameCataloguePrice(page: number, name: string, catalogue: string, price1: number, price2: number): Observable<any> {
     return this.http.get(this.API + '/search?' +
       'page=' + page + '&size=3&name=' + name + '&catalogue=' + catalogue + '&price1=' + price1 + '&price2=' + price2);
-  //  search?page=0&size=9&name=iph&catalogue=Công nghệ&price1=0&price2=9000000
+    //  search?page=0&size=9&name=iph&catalogue=Công nghệ&price1=0&price2=9000000
   }
+
   getAllProductByCatalogue(page: number, catalogue: string): Observable<any> {
     return this.http.get(this.API + '/search?page=' + page + '&size=6' + '&catalogue=' + catalogue);
+  }
+
+  getCurrentPriceById(id: number): Observable<any> {
+    return this.http.get(this.API + '/current-price?id=' + id);
+  }
+  getTopFive(id: number): Observable<any> {
+    return this.http.get(this.API + '/top-five?id=' + id);
   }
 }
