5a89b7390790bb38e62673c584273c37eebf1cb6
Shinya9082 <anhtuandtu1993@gmail.com>
2020-03-24 21:10:31 +0700

    product-comment.component first look

--- src/app/products/product-comment/product-comment.component.ts
+++ src/app/products/product-comment/product-comment.component.ts
@@ -1,4 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+import {CookieStorageService} from '../../services/auth/cookie-storage.service';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 export class Comment {
   displayName: string;
@@ -13,9 +17,56 @@ export class Comment {
 })
 export class ProductCommentComponent implements OnInit {
   @Input() private productID: string;
-  constructor() { }
+  list: Observable<Comment[]>;
+  private itemsCollection: AngularFirestoreCollection<Comment>;
+  commentForm: FormGroup;
+  userName;
+  size = 0;
+  getAll = false;
 
-  ngOnInit(): void {
+  constructor(
+    private afs: AngularFirestore,
+    private cookieStorageService: CookieStorageService,
+    private formBuilder: FormBuilder
+  ) {
   }
 
+  ngOnInit(): void {
+    this.commentForm = this.formBuilder.group({
+      displayName: [''],
+      creationDate: [''],
+      message: ['']
+    });
+    this.userName = this.cookieStorageService.getUsername();
+    this.getComment();
+  }
+
+  getComment() {
+    this.itemsCollection =
+      this.afs.collection('product')
+        .doc(this.productID)
+        .collection<Comment>('comment', ref => ref.limit(5)
+          .orderBy('creationDate', 'desc'));
+    this.list = this.itemsCollection.valueChanges();
+  }
+
+  getAllComment() {
+    this.getAll = true;
+    this.itemsCollection =
+      this.afs.collection('product')
+        .doc(this.productID)
+        .collection<Comment>('comment', ref => ref.orderBy('creationDate', 'desc'));
+    this.list = this.itemsCollection.valueChanges();
+  }
+
+  saveComment() {
+    if (this.cookieStorageService.getUsername() !== '') {
+      this.commentForm.controls.displayName.setValue(this.cookieStorageService.getUsername());
+      this.commentForm.controls.creationDate.setValue(Date.now());
+      this.afs.collection('product')
+        .doc(this.productID)
+        .collection('comment')
+        .add(this.commentForm.value);
+    }
+  }
 }
