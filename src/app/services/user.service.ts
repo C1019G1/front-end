6042daaec4fbbe210492d3c58a00ef553c891d4c
Shinya9082 <anhtuandtu1993@gmail.com>
2020-03-24 09:54:27 +0700

    update

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
@@ -7,17 +7,27 @@ import {Observable} from 'rxjs';
 })
 export class UserService {
   public API = 'http://localhost:8080/';
-  constructor(public http: HttpClient) { }
+
+  constructor(public http: HttpClient) {
+  }
+
   register(user): Observable<any> {
     return this.http.post(this.API + 'register', user);
   }
+
   getLoginHistory(username): Observable<any> {
     return this.http.post(this.API + 'login-history', username);
   }
+
   changePassword(changePasswordForm): Observable<any> {
     return this.http.post(this.API + 'change-password', changePasswordForm);
   }
+
   resetPassword(resetPasswordForm): Observable<any> {
     return this.http.post(this.API + 'reset-password', resetPasswordForm);
+  }
+
+  getCart(username: string): Observable<any> {
+    return this.http.get(this.API + 'cart?userName=' + username);
   }
 }
